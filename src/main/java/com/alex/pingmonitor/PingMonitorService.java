309e15139a5dec7f7c4d2918efb79f41df527560
Alexandru Sarbu <alex@sarbu.de>
2023-09-20 02:52:30 +0200

    downgraded to java 11

--- src/main/java/com/alex/pingmonitor/PingMonitorService.java
+++ src/main/java/com/alex/pingmonitor/PingMonitorService.java
@@ -1,11 +1,10 @@
 package com.alex.pingmonitor;
 
-import jakarta.ws.rs.GET;
-import jakarta.ws.rs.Path;
-import jakarta.ws.rs.Produces;
-import jakarta.ws.rs.core.MediaType;
-
 import javax.sql.DataSource;
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
 
 @Path("/ping-monitor")
 public class PingMonitorService {
